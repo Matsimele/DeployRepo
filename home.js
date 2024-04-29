fe325a4c0d88efdeec52220a8e8c5206bc22295c
mish9408@gmail.com <mish9408@gmail.com>
2024-04-29 22:27:59 +0200

    Final code 2

---
+++ home.js
@@ -0,0 +1,54 @@
+
+
+var header ="Join our Community";
+var tit = "30-day, hassle-free money back quarantee";
+var par1 = 'Get access to our full library of tutorials along with expert code reviews.\nPerfect for any developers who are serious about honing their skills.';
+var subs ="Monthly Subscription";
+var why ="Why Us";
+var amnt ="$29"
+var expl ="Full access for less than $1 a day";
+var line1 ="Tutorials by industry experts";
+var line2="Peer & expert code review";
+var line3="Coding exeecises";
+var line4= "Access to our GitHub repos";
+var line5= "Community forum";
+var line6 = "Flashcard decks";
+var line6 = "New videos every week";
+
+
+var head = document.getElementById("heading");
+var hassle = document.getElementById("hassle");
+var tabheds =document.getElementById("para");
+var subscrpt =document.getElementById("Subscription");
+var us = document.getElementById("Why");
+var amunt = document.getElementById("amount");
+var exp = document.getElementById("explanaition");
+var lin1 = document.getElementById("code1");
+var lin2 = document.getElementById("code2");
+var lin3 = document.getElementById("code3");
+var lin4 = document.getElementById("code4");
+var lin5 = document.getElementById("code5");
+var lin6 = document.getElementById("code6");
+
+head.textContent=header;
+hassle.textContent=tit;
+tabheds.textContent= par1;
+subscrpt.textContent=subs;
+us.textContent=why;
+amunt.textContent=amnt;
+exp.textContent=expl;
+lin1.textContent=line1;
+lin2.textContent=line2;
+lin3.textContent=line3;
+lin4.textContent=line4;
+lin5.textContent=line5;
+lin6.textContent=line6;
+
+
+
+//var part2 = "Perfect for any developers who are serious about honing their skills."
+
+
+
+
+     
